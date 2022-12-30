af05ed74d9f8335bea54c62fbf13f98223c4d64f
Zip0 <kajetan.dziebaj@gmail.com>
2022-12-30 18:18:40 +0100

    added filling table from array of objects

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+echo 'nigger xD';
+
+
+?>
--- js/calendar.js
+++ js/calendar.js
@@ -1,26 +1,50 @@
 
-const events = {
-    0:{date: '01.01.2023', title: "New Year's Day", note: 'Buy champagne', priority: 'less important'},
-    1:{date: '24.01.2023', title: 'Day Grandmother and Grandfather', note: 'Buy flowers and making a card', priority: 'important'},
-    2:{date: '09.02.2023', title: 'My Birthday', note: 'Buy cake', priority: 'important'},
-    3:{date: '14.02.2023', title: 'Valentine Day', note: 'Cook a delicious dinner', priority: 'less important'},
-    4:{date: '08.03.2023', title: "Women's Day", note: 'Spa time', priority: 'less important'},
-    5:{date: '01.05.2023', title: 'Labour Day', note: 'Day off', priority: 'important'},
-    6:{date: '03.05.2023', title: "Constitution of May 3", note: 'Procession in the market', priority: 'important'},
-    7:{date: '01.06.2023', title: "Children's Day", note: 'Buy toys', priority: 'less important'},
-    8:{date: '01.08.2023', title: 'National Day of Remembrance of the Warsaw Uprising', note: 'Commemoration day', priority: 'important'},
-    9:{date: '09.04.2023', title: 'EasterDay', note: 'Prepare the eggs', priority: 'less important'},
-    10:{date: '11.11.2023', title: 'National Independence Day', note: 'procession through the city', priority: 'important'},
-    11:{date: '24.12.2023', title: 'christmas eve', note: 'Christmas dinner', priority: 'important'},
-    12:{date: '25.12.2023', title: "Christmas", note: 'Family time', priority: 'important'},
-    13:{date: '10.04.2023', title: 'Wet Monday', note: 'prepare a water gun', priority: 'less important'},
-    14:{date: '27.10.2023', title: 'World Occupational Therapy Day.', note: 'My work', priority: 'less important'},
-    15:{date: '03.12.2023', title: 'Day of People with Disabilities', note: 'Organizing an exhibition of works by people with disabilities', priority: 'less important'},
-    16:{date: '20.08.2023', title: 'World Mosquito Day', note: 'buy a deet preparation', priority: 'common'},
-    17:{date: '21.06.2023', title: 'Poop night', note: 'go to the fire', priority: 'less important'},
-    18:{date: '26.05.2023', title: "Mother's Day", note: 'Buy flowers', priority: 'important'},
-    19:{date: '07.04.2023', title: "Beaver Day", note: 'walk by the dams', priority: 'common'},
-    20:{date: '17.02.2023', title: 'Cat Day', note: 'petting a cat', priority: 'important all year'},
-    
+const events = [
+    {date: '01.01.2023', title: "New Year's Day", note: 'Buy champagne', priority: 'less important'},
+    {date: '24.01.2023', title: 'Day Grandmother and Grandfather', note: 'Buy flowers and making a card', priority: 'important'},
+    {date: '09.02.2023', title: 'My Birthday', note: 'Buy cake', priority: 'important'},
+    {date: '14.02.2023', title: 'Valentine Day', note: 'Cook a delicious dinner', priority: 'less important'},
+    {date: '08.03.2023', title: "Women's Day", note: 'Spa time', priority: 'less important'},
+    {date: '01.05.2023', title: 'Labour Day', note: 'Day off', priority: 'important'},
+    {date: '03.05.2023', title: "Constitution of May 3", note: 'Procession in the market', priority: 'important'},
+    {date: '01.06.2023', title: "Children's Day", note: 'Buy toys', priority: 'less important'},
+    {date: '01.08.2023', title: 'National Day of Remembrance of the Warsaw Uprising', note: 'Commemoration day', priority: 'important'},
+    {date: '09.04.2023', title: 'EasterDay', note: 'Prepare the eggs', priority: 'less important'},
+    {date: '11.11.2023', title: 'National Independence Day', note: 'procession through the city', priority: 'important'},
+    {date: '24.12.2023', title: 'christmas eve', note: 'Christmas dinner', priority: 'important'},
+    {date: '25.12.2023', title: "Christmas", note: 'Family time', priority: 'important'},
+    {date: '10.04.2023', title: 'Wet Monday', note: 'prepare a water gun', priority: 'less important'},
+    {date: '27.10.2023', title: 'World Occupational Therapy Day.', note: 'My work', priority: 'less important'},
+    {date: '03.12.2023', title: 'Day of People with Disabilities', note: 'Organizing an exhibition of works by people with disabilities', priority: 'less important'},
+    {date: '20.08.2023', title: 'World Mosquito Day', note: 'buy a deet preparation', priority: 'common'},
+    {date: '21.06.2023', title: 'Poop night', note: 'go to the fire', priority: 'less important'},
+    {date: '26.05.2023', title: "Mother's Day", note: 'Buy flowers', priority: 'important'},
+    {date: '07.04.2023', title: "Beaver Day", note: 'walk by the dams', priority: 'common'},
+    {date: '17.02.2023', title: 'Cat Day', note: 'petting a cat', priority: 'important all year'}
+    ]
+
+fillTable(events); //tu odpalamy funkcję zapełniającą tabelę jeśli chcesz zmienić co się wyświetla w tabeli zmieniasz zbiór events
+//swoją drogą zbiór events jest teraz zbiorem obiektów
+//działaj i pytaj
+
+
+
+
+function fillTable(events) {
+    for (i = 0; i < events.length; i++) {
+        console.log('add row')
+        addRow('table', events[i]);
     }
-    document.querySelector('#eventTable').innerHTML = events
+}
+
+function addRow(eventTable, event) {
+    let tableRef = document.getElementById('eventTable');
+    console.log('tableRef', tableRef)
+    let newRow = tableRef.insertRow(-1);
+    for (let prop in event) {
+        console.log('prop')
+        let newCell = newRow.insertCell(-1);
+        let newText = document.createTextNode(event[prop]);
+        newCell.appendChild(newText);
+    }
+}
