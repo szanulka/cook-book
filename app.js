4ffd83487d06741e34eab791eb0b6c1984f6e0d3
Zip0 <kajetan.dziebaj@gmail.com>
2023-01-17 09:14:51 +0100

    learning express

--- app.js
+++ app.js
@@ -6,24 +6,36 @@ const app = express()
 const hostname = '127.0.0.1';
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(__dirname + '/css'))
 app.use('/js', express.static(__dirname + '/js'))
 app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile)
-app.get('', (req, res) => {
-    res.render('index', { text: 'Hey' })
+
+app.get('' || '/index.html', (req, res) => {
+    // res.render('index')
+    res.sendFile(__dirname + '/views/index.html')
 })
 
-app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/views/about.html')
+app.get('/dessert.html', (req, res) => {
+    res.sendFile(__dirname + '/views/dessert.html')
+})
+app.get('/soups.html', (req, res) => {
+    res.sendFile(__dirname + '/views/soups.html')
+})
+app.get('/calendar.html', (req, res) => {
+    res.sendFile(__dirname + '/views/calendar.html')
+})
+
+app.get(route = 'index.html', (req, res) => {
+    res.sendFile(__dirname + route)
 })
 
 app.listen(port, () => console.info(`App listening on port ${port}`))
 
 let mysql = require('mysql');
-let con = mysql.createConnection({
+let connection = mysql.createConnection({
     host: "localhost",
     database: "cookbook",
     user: "root",
@@ -32,10 +44,10 @@ let con = mysql.createConnection({
 
 let sql = "SELECT * FROM hello";
 
-con.connect(function(err) {
+connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    con.query(sql, function (err, result) {
+    connection.query(sql, function (err, result) {
         if (err) throw err;
         console.log("Result: " + JSON.stringify(result));
     });
