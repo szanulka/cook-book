ddfc9b34bdce3bcd4b1ee0ec759218a6646a31bf
Luiza Krasnowska <luizakrasnowska@gmail.com>
2023-03-06 10:27:01 +0000

    invalid fivicon, table priority, font-family in modal

--- cookbook.session.sql
+++ cookbook.session.sql
@@ -1,3 +1,8 @@
-select * from event;
+CREATE TABLE priority (
+    priority_id int auto_increment,
+    priority_name varchar(255) not null,
+    priority_price date,
 
+    primary key(priority_id)
+);
 
